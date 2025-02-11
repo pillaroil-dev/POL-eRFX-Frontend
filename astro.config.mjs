d052d5115c99b49dfcb67da265d8684a6134e22f
danielDozie <placmedia@gmail.com>
2025-02-11 15:37:41 +0100

    changed port back to 3000

--- astro.config.mjs
+++ astro.config.mjs
@@ -43,19 +43,19 @@ export default defineConfig({
   },
   site: 'https://app.polrfx.ng',
   server:{
-        port: 4321,
+        port: 3000,
         host: '0.0.0.0'
     },
   vite:{
     server: {
-      port: 4321,
+      port: 3000,
       host: true  // Equivalent to '0.0.0.0'
     },
     ssr: {
         noExternal: ['path-to-regexp'],
       },
     preview: {
-        port: 4321,
+        port: 3000,
         host: true
       }
     },
