274f8be7cfea93efd9a1c924fc4a01321188b73d
danielDozie <placmedia@gmail.com>
2025-02-11 11:31:13 +0100

    fixed host issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -44,16 +44,20 @@ export default defineConfig({
   site: 'https://app.polrfx.ng',
   server:{
         port: 3000,
-        host: true
+        host: '0.0.0.0'
     },
   vite:{
-        ssr: {
-            noExternal: ['path-to-regexp'],
-          },
-        preview: {
-            port: 3000,
-            host: true
-        }
+    server: {
+      port: 3000,
+      host: true  // Equivalent to '0.0.0.0'
+    },
+    ssr: {
+        noExternal: ['path-to-regexp'],
+      },
+    preview: {
+        port: 3000,
+        host: true
+      }
     },
     build: {
       rollupOptions: {
