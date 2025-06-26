6c5f526a8f3551ee142e1ce6eca56026f6792e1b
danielDozie <placmedia@gmail.com>
2025-06-26 01:02:13 +0100

    Set Minio client to use SSL as false in configuration

--- src/pages/api/v1/util/minio-api.ts
+++ src/pages/api/v1/util/minio-api.ts
@@ -4,7 +4,7 @@ import * as Minio from 'minio'
 const minioClient = new Minio.Client({
     endPoint: 's3connect.polrfx.ng',
     port: 9000,
-    useSSL: import.meta.env.MINIO_USESSL || true,
+    useSSL: false,
     accessKey: import.meta.env.MINIO_ACCESS_KEY,
     secretKey: import.meta.env.MINIO_SECRET_KEY,
 });
