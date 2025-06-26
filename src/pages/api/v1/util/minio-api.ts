219e760945bf6c82f6fca38cc505c21f636844dc
danielDozie <placmedia@gmail.com>
2025-06-26 15:05:30 +0100

    Update Minio client configuration to use a specific endpoint and enable SSL

--- src/pages/api/v1/util/minio-api.ts
+++ src/pages/api/v1/util/minio-api.ts
@@ -2,9 +2,9 @@ import type { APIRoute } from "astro";
 import * as Minio from 'minio'
 
 const minioClient = new Minio.Client({
-    endPoint: import.meta.env.SERVER_IP,
+    endPoint: 's3connect.polrfx.ng',
     port: 9000,
-    useSSL: false,
+    useSSL: true,
     accessKey: import.meta.env.MINIO_ACCESS_KEY,
     secretKey: import.meta.env.MINIO_SECRET_KEY,
 });
