a80d4c87e6e6dbbb6051ef01644529744c25f4f0
danielDozie <placmedia@gmail.com>
2025-06-28 03:33:02 +0100

    Refactor FileUploader component to use alert messages from a centralized object and improve loading indicator styling; update Minio client configuration to disable SSL.

--- src/components/ui/react/uploader.tsx
+++ src/components/ui/react/uploader.tsx
@@ -8,11 +8,18 @@ export const FileUploader = () => {
     const [loading, setLoading] = useState(false);
     const [localData, setLocalData] = useState([]);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setAlert('');
-        }, 2500)
-    }, [alert]);
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setAlert('');
+    //     }, 2500)
+    // }, [alert]);
+
+    const alertMsg = {
+        success: "Upload successful ✓",
+        failed: "Upload failed ✕",
+        delete: "File deleted ✓",
+        deleteFailed: "Delete failed ✕"
+    }
 
     // called every time a file's `status` changes
     const handleChangeStatus = ({ meta, file }: any, status: string, files: any[]) => {
@@ -32,10 +39,10 @@ export const FileUploader = () => {
                 }));
                 responses.forEach(response => {
                     if (response.ok) {
-                        setAlert('Upload successful');
+                        setAlert(alertMsg.success);
                     } else {
-                        setAlert('Upload failed');
-                        console.error('Upload failed', response);
+                        setAlert(alertMsg.failed);
+                        console.error(alertMsg.failed, response);
                     }
                 });
                 setLoading(false);
@@ -63,10 +70,10 @@ export const FileUploader = () => {
                     const response  = await removeItemByName({ objectName: meta.name });
                     if (!response.error) {
                         setLoading(false)
-                        setAlert('File deleted!');
+                        setAlert(alertMsg.delete);
                     } else {
                         setLoading(false)
-                        setAlert('Delete failed');
+                        setAlert(alertMsg.deleteFailed);
                     }
                 })();
                 setLocalData(prevData => prevData.filter(item => item.name !== meta.name));
@@ -100,12 +107,12 @@ export const FileUploader = () => {
                 }}
             />
             <div className="flex justify-center items-center text-green-500 text-xs text-right">
-                {loading && <svg className="animate-spin -ml-1 mr-3 h-3 w-3 text-gree-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                {loading && <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
                 }
-                <p className='text-green-600 text-xs text-right font-semibold'>{loading ? 'Please wait...' : alert}</p>
+                <p className={`${(alertMsg.success || alertMsg.delete ? "text-green-600" : "text-red-600")} text-sm text-right font-bold`}>{loading ? 'Please wait...' : alert}</p>
         </div>
         </>
 
